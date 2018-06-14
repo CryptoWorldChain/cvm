25d9b8618ec559fd6970c0e355eb44b09f7dd4e2
sean <sean@finchain360.com>
2018-06-14 15:57:01 +0800

    add save code

--- evmapi/src/main/java/org/brewchain/evm/api/EvmApi.java
+++ evmapi/src/main/java/org/brewchain/evm/api/EvmApi.java
@@ -14,6 +14,8 @@ import com.google.protobuf.InvalidProtocolBufferException;
 
 public interface EvmApi {
 
+	public void saveCode(byte[] code, byte[] address);
+
 	/**
 	 * 获取用户账户
 	 * 
@@ -21,15 +23,16 @@ public interface EvmApi {
 	 * @return
 	 */
 	public Account GetAccount(byte[] addr);
-	
+
 	/**
 	 * 创建账户
+	 * 
 	 * @param address
 	 * @param pubKey
 	 * @return
 	 */
 	public Account CreateAccount(byte[] address, byte[] pubKey);
-	
+
 	/**
 	 * 获取用户账户，如果用户不存在，则创建账户
 	 * 
@@ -37,9 +40,10 @@ public interface EvmApi {
 	 * @return
 	 */
 	public Account GetAccountOrCreate(byte[] addr);
-	
+
 	/**
 	 * 创建合约账户
+	 * 
 	 * @param address
 	 * @param pubKey
 	 * @param code
@@ -47,9 +51,10 @@ public interface EvmApi {
 	 * @return
 	 */
 	public Account CreateContract(byte[] address, byte[] pubKey, byte[] code, byte[] exdata);
-	
+
 	/**
 	 * 创建账户
+	 * 
 	 * @param address
 	 * @param pubKey
 	 * @param max
@@ -62,21 +67,22 @@ public interface EvmApi {
 	 */
 	public Account CreateAccount(byte[] address, byte[] pubKey, long max, long acceptMax, int acceptLimit,
 			List<ByteString> addresses, byte[] code, byte[] exdata);
-	
+
 	/**
 	 * 创建联合账户
+	 * 
 	 * @param oAccount
 	 * @return
 	 */
 	public Account CreateUnionAccount(Account oAccount);
-	
+
 	/**
 	 * 移除账户。删除后不可恢复。
 	 * 
 	 * @param address
 	 */
 	public void DeleteAccount(byte[] address);
-	
+
 	/**
 	 * 账户是否存在
 	 * 
@@ -85,7 +91,7 @@ public interface EvmApi {
 	 * @throws Exception
 	 */
 	public boolean isExist(byte[] addr);
-	
+
 	/**
 	 * Nonce自增1
 	 * 
@@ -94,7 +100,7 @@ public interface EvmApi {
 	 * @throws Exception
 	 */
 	public int IncreaseNonce(byte[] addr);
-	
+
 	/**
 	 * 增加用户账户余额
 	 * 
@@ -104,14 +110,15 @@ public interface EvmApi {
 	 * @throws Exception
 	 */
 	public long addBalance(byte[] addr, long balance);
-	
+
 	/**
 	 * 获取账户余额
+	 * 
 	 * @param addr
 	 * @return
 	 */
 	public long getBalance(byte[] addr);
-	
+
 	/**
 	 * 增加用户代币账户余额
 	 * 
@@ -121,9 +128,9 @@ public interface EvmApi {
 	 * @throws Exception
 	 */
 	public long addTokenBalance(byte[] addr, String token, long balance);
-	
+
 	public long addTokenLockBalance(byte[] addr, String token, long balance);
-	
+
 	/**
 	 * 增加加密Token账户余额
 	 * 
@@ -134,9 +141,9 @@ public interface EvmApi {
 	 * @throws Exception
 	 */
 	public long addCryptoBalance(byte[] addr, String symbol, AccountCryptoToken.Builder token);
-	
+
 	public long newCryptoBalances(byte[] addr, String symbol, ArrayList<AccountCryptoToken.Builder> tokens);
-	
+
 	/**
 	 * 移除加密Token
 	 * 
@@ -146,7 +153,7 @@ public interface EvmApi {
 	 * @return
 	 */
 	public long removeCryptoBalance(byte[] addr, String symbol, byte[] hash);
-	
+
 	/**
 	 * 设置用户账户Nonce
 	 * 
@@ -156,14 +163,15 @@ public interface EvmApi {
 	 * @throws Exception
 	 */
 	public int setNonce(byte[] addr, int nonce);
-	
+
 	/**
 	 * 是否是合约账户
+	 * 
 	 * @param addr
 	 * @return
 	 */
 	public boolean isContract(byte[] addr);
-	
+
 	/**
 	 * 获取用户账户Nonce
 	 * 
@@ -172,7 +180,7 @@ public interface EvmApi {
 	 * @throws Exception
 	 */
 	public int getNonce(byte[] addr);
-	
+
 	/**
 	 * 获取用户Token账户的Balance
 	 * 
@@ -181,9 +189,9 @@ public interface EvmApi {
 	 * @throws Exception
 	 */
 	public long getTokenBalance(byte[] addr, String token);
-	
+
 	public long getTokenLockedBalance(byte[] addr, String token);
-	
+
 	/**
 	 * 获取加密Token账户的余额
 	 * 
@@ -193,7 +201,7 @@ public interface EvmApi {
 	 * @throws Exception
 	 */
 	public List<AccountCryptoToken> getCryptoTokenBalance(byte[] addr, String symbol);
-	
+
 	/**
 	 * 生成加密Token方法。 调用时必须确保symbol不重复。
 	 * 
@@ -204,9 +212,9 @@ public interface EvmApi {
 	 * @throws Exception
 	 */
 	public void generateCryptoToken(byte[] addr, String symbol, String[] name, String[] code);
-	
+
 	public void ICO(byte[] addr, String token);
-	
+
 	/**
 	 * 判断token是否已经发行
 	 * 
@@ -215,9 +223,9 @@ public interface EvmApi {
 	 * @throws Exception
 	 */
 	public boolean isExistsToken(String token);
-	
+
 	public void putAccountValue(byte[] addr, AccountValue oAccountValue);
-	
+
 	/**
 	 * 保存交易方法。 交易不会立即执行，而是等待被广播和打包。只有在Block中的交易，才会被执行。 交易签名规则 1. 清除signatures 2.
 	 * txHash=ByteString.EMPTY 3. 签名内容=oMultiTransaction.toByteArray()
@@ -226,9 +234,9 @@ public interface EvmApi {
 	 * @throws Exception
 	 */
 	public ByteString CreateMultiTransaction(MultiTransaction.Builder oMultiTransaction);
-	
+
 	public ByteString CreateGenesisMultiTransaction(MultiTransaction.Builder oMultiTransaction);
-	
+
 	/**
 	 * 广播交易方法。 交易广播后，节点收到的交易会保存在本地db中。交易不会立即执行，而且不被广播。只有在Block中的交易，才会被执行。
 	 * 
@@ -236,14 +244,14 @@ public interface EvmApi {
 	 * @throws Exception
 	 */
 	public void SyncTransaction(MultiTransaction.Builder oMultiTransaction);
-	
+
 	/**
 	 * 交易执行。交易只在接收到Block后才会被执行
 	 * 
 	 * @param oTransaction
 	 */
 	public void ExecuteTransaction(LinkedList<MultiTransaction> oMultiTransactions);
-	
+
 	/**
 	 * 从待广播交易列表中，查询出交易进行广播。广播后，不论是否各节点接收成功，均放入待打包列表
 	 * 
@@ -252,7 +260,7 @@ public interface EvmApi {
 	 * @throws InvalidProtocolBufferException
 	 */
 	public List<MultiTransaction> getWaitSendTx(int count);
-	
+
 	/**
 	 * 从待打包交易列表中，查询出等待打包的交易。
 	 * 
@@ -261,9 +269,9 @@ public interface EvmApi {
 	 * @throws InvalidProtocolBufferException
 	 */
 	public LinkedList<MultiTransaction> getWaitBlockTx(int count);
-	
+
 	public void removeWaitBlockTx(byte[] txHash);
-	
+
 	/**
 	 * 根据交易Hash，返回交易实体。
 	 * 
@@ -272,8 +280,7 @@ public interface EvmApi {
 	 * @throws Exception
 	 */
 	public MultiTransaction GetTransaction(byte[] txHash);
-	
-	
+
 	/**
 	 * 交易签名方法。该方法未实现
 	 * 
@@ -282,7 +289,7 @@ public interface EvmApi {
 	 * @throws Exception
 	 */
 	public void Signature(List<String> privKeys, MultiTransaction.Builder oTransaction);
-	
+
 	/**
 	 * 获取交易签名后的Hash
 	 * 
@@ -290,7 +297,7 @@ public interface EvmApi {
 	 * @throws Exception
 	 */
 	public void getTransactionHash(MultiTransaction.Builder oTransaction);
-	
+
 	/**
 	 * 校验并保存交易。该方法不会执行交易，用户的账户余额不会发生变化。
 	 * 
@@ -298,20 +305,20 @@ public interface EvmApi {
 	 * @throws Exception
 	 */
 	public MultiTransaction verifyAndSaveMultiTransaction(MultiTransaction.Builder oMultiTransaction);
-	
+
 	/**
 	 * 验证签名
+	 * 
 	 * @param pubKey
 	 * @param signature
 	 * @param tx
 	 */
 	public void verifySignature(String pubKey, String signature, byte[] tx);
-	
+
 	public void setTransactionDone(byte[] txHash);
-	
+
 	public void setTransactionError(byte[] txHash);
-	
+
 	public byte[] getContractAddressByTransaction(MultiTransaction oMultiTransaction);
-	
-	
+
 }
