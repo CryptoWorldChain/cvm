722146d8aacaf0550cfc6a23110f71c4ed3830f1
leo <bulenessdragon@sina.com>
2018-08-14 19:13:18 +0800

    code up

--- rcvm/src/main/java/org/brewchain/rcvm/exec/zksnark/BN128G1.java
+++ rcvm/src/main/java/org/brewchain/rcvm/exec/zksnark/BN128G1.java
@@ -2,36 +2,38 @@ package org.brewchain.rcvm.exec.zksnark;
 
 public class BN128G1 extends BN128Fp {
 
-    BN128G1(BN128<Fp> p) {
-        super(p.x, p.y, p.z);
-    }
+	BN128G1(BN128<Fp> p) {
+		super(p.x, p.y, p.z);
+	}
 
-    @Override
-    public BN128G1 toAffine() {
-        return new BN128G1(super.toAffine());
-    }
+	@Override
+	public BN128G1 toAffine() {
+		return new BN128G1(super.toAffine());
+	}
 
-    /**
-     * Checks whether point is a member of subgroup,
-     * returns a point if check has been passed and null otherwise
-     */
-    public static BN128G1 create(byte[] x, byte[] y) {
+	/**
+	 * 
+	 * Checks whether point is a member of subgroup, returns a point if check has
+	 * been passed and null otherwise
+	 */
+	public static BN128G1 create(byte[] x, byte[] y) {
 
-        BN128<Fp> p = BN128Fp.create(x, y);
+		BN128<Fp> p = BN128Fp.create(x, y);
 
-        if (p == null) return null;
+		if (p == null)
+			return null;
 
-        if (!isGroupMember(p)) return null;
+		if (!isGroupMember(p))
+			return null;
 
-        return new BN128G1(p);
-    }
+		return new BN128G1(p);
+	}
 
-    /**
-     * Formally we have to do this check
-     * but in our domain it's not necessary,
-     * thus always return true
-     */
-    private static boolean isGroupMember(BN128<Fp> p) {
-        return true;
-    }
+	/**
+	 * Formally we have to do this check but in our domain it's not necessary, thus
+	 * always return true
+	 */
+	private static boolean isGroupMember(BN128<Fp> p) {
+		return true;
+	}
 }
